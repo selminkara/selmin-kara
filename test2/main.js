8cb85e209cc188226b4bea7f36e061db91882418
Christine <christine-xia@hotmail.com>
2019-12-27 21:00:09 -0500

    Website tests

---
+++ test2/main.js
@@ -0,0 +1,137 @@
+$('.box.bio').click(function(){
+	// $(this).css({'z-index':'100'})
+	// $('.box.research').css({'z-index':'99'})
+	// $('.box.pub').css({'z-index':'98'})
+	// $('.box.teach').css({'z-index':'97'})
+	// $('.box.events').css({'z-index':'96'})
+	// $('.box.contact').css({'z-index':'95'})
+
+	$('.box').css({'background':'black'})
+	$('.desc').css({'color':'white'})
+	$('a').css({'color':'white'})
+
+	$(this).css({'height':'50vh','overflow':'auto','background':'white'})
+	$('.box.bio .desc').css({'color':'black'})
+	$('.box.bio a').css({'color':'black'})
+
+	$('.box.research').css({'height':'auto','overflow':'hidden'})
+	$('.box.pub').css({'height':'auto','overflow':'hidden'})
+	$('.box.teach').css({'height':'auto','overflow':'hidden'})
+	$('.box.events').css({'height':'auto','overflow':'hidden'})
+	$('.box.contact').css({'height':'auto','overflow':'hidden'})
+})
+
+$('.box.research').click(function(){
+	// $(this).css({'z-index':'100'})
+	// $('.box.bio').css({'z-index':'99'})
+	// $('.box.pub').css({'z-index':'98'})
+	// $('.box.teach').css({'z-index':'97'})
+	// $('.box.events').css({'z-index':'96'})
+	// $('.box.contact').css({'z-index':'95'})
+
+	$('.box').css({'background':'black'})
+	$('.desc').css({'color':'white'})
+	$('a').css({'color':'white'})
+
+	$(this).css({'height':'50vh','overflow':'auto','background':'white'})
+	$('.box.research .desc').css({'color':'black'})
+	$('.box.research a').css({'color':'black'})
+
+	$('.box.bio').css({'height':'auto','overflow':'hidden'})
+	$('.box.pub').css({'height':'auto','overflow':'hidden'})
+	$('.box.teach').css({'height':'auto','overflow':'hidden'})
+	$('.box.events').css({'height':'auto','overflow':'hidden'})
+	$('.box.contact').css({'height':'auto','overflow':'hidden'})
+})
+
+$('.box.pub').click(function(){
+	// $(this).css({'z-index':'100'})
+	// $('.box.bio').css({'z-index':'99'})
+	// $('.box.research').css({'z-index':'98'})
+	// $('.box.teach').css({'z-index':'97'})
+	// $('.box.events').css({'z-index':'96'})
+	// $('.box.contact').css({'z-index':'95'})
+
+	$('.box').css({'background':'black'})
+	$('.desc').css({'color':'white'})
+	$('a').css({'color':'white'})
+
+	$(this).css({'height':'50vh','overflow':'auto','background':'white'})
+	$('.box.pub .desc').css({'color':'black'})
+	$('.box.pub a').css({'color':'black'})
+
+	$('.box.bio').css({'height':'auto','overflow':'hidden'})
+	$('.box.research').css({'height':'auto','overflow':'hidden'})
+	$('.box.teach').css({'height':'auto','overflow':'hidden'})
+	$('.box.events').css({'height':'auto','overflow':'hidden'})
+	$('.box.contact').css({'height':'auto','overflow':'hidden'})
+})
+
+$('.box.teach').click(function(){
+	// $(this).css({'z-index':'100'})
+	// $('.box.bio').css({'z-index':'99'})
+	// $('.box.research').css({'z-index':'98'})
+	// $('.box.pub').css({'z-index':'97'})
+	// $('.box.events').css({'z-index':'96'})
+	// $('.box.contact').css({'z-index':'95'})
+
+	$('.box').css({'background':'black'})
+	$('.desc').css({'color':'white'})
+	$('a').css({'color':'white'})
+
+	$(this).css({'height':'50vh','overflow':'auto','background':'white'})
+	$('.box.teach .desc').css({'color':'black'})
+	$('.box.teach a').css({'color':'black'})
+
+	$('.box.bio').css({'height':'auto','overflow':'hidden'})
+	$('.box.research').css({'height':'auto','overflow':'hidden'})
+	$('.box.pub').css({'height':'auto','overflow':'hidden'})
+	$('.box.events').css({'height':'auto','overflow':'hidden'})
+	$('.box.contact').css({'height':'auto','overflow':'hidden'})
+})
+
+$('.box.events').click(function(){
+	// $(this).css({'z-index':'100'})
+	// $('.box.bio').css({'z-index':'99'})
+	// $('.box.research').css({'z-index':'98'})
+	// $('.box.pub').css({'z-index':'97'})
+	// $('.box.teach').css({'z-index':'96'})
+	// $('.box.contact').css({'z-index':'95'})
+
+	$('.box').css({'background':'black'})
+	$('.desc').css({'color':'white'})
+	$('a').css({'color':'white'})
+
+	$(this).css({'height':'50vh','overflow':'auto','background':'white'})
+	$('.box.events .desc').css({'color':'black'})
+	$('.box.events a').css({'color':'black'})
+
+	$('.box.bio').css({'height':'auto','overflow':'hidden'})
+	$('.box.research').css({'height':'auto','overflow':'hidden'})
+	$('.box.pub').css({'height':'auto','overflow':'hidden'})
+	$('.box.teach').css({'height':'auto','overflow':'hidden'})
+	$('.box.contact').css({'height':'auto','overflow':'hidden'})
+})
+
+$('.box.contact').click(function(){
+	// $(this).css({'z-index':'100'})
+	// $('.box.bio').css({'z-index':'99'})
+	// $('.box.research').css({'z-index':'98'})
+	// $('.box.pub').css({'z-index':'97'})
+	// $('.box.teach').css({'z-index':'96'})
+	// $('.box.events').css({'z-index':'95'})
+
+	$('.box').css({'background':'black'})
+	$('.desc').css({'color':'white'})
+	$('a').css({'color':'white'})
+
+	$(this).css({'height':'50vh','overflow':'auto','background':'white'})
+	$('.box.contact .desc').css({'color':'black'})
+	$('.box.contact a').css({'color':'black'})
+
+	$('.box.bio').css({'height':'auto','overflow':'hidden'})
+	$('.box.research').css({'height':'auto','overflow':'hidden'})
+	$('.box.pub').css({'height':'auto','overflow':'hidden'})
+	$('.box.teach').css({'height':'auto','overflow':'hidden'})
+	$('.box.events').css({'height':'auto','overflow':'hidden'})
+})
